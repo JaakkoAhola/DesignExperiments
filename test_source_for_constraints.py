4c775e37d68477c71052b8f2f3d7c3ed918661ef
Jaakko Ahola <jaakko.ahola@fmi.fi>
2021-12-02 17:31:11 +0200

    modified:   test_source_for_constraints.py 			- added intermediate printing 			- added saving pure source

--- test_source_for_constraints.py
+++ test_source_for_constraints.py
@@ -10,7 +10,7 @@ import os
 import sys
 import time
 from datetime import datetime
-
+import pandas
 sys.path.append(os.environ["LESMAINSCRIPTS"])
 from Data import Data
 sys.path.append(os.environ["CODEX"] + "/LES-superfolder/LES-emulator-02postpros")
@@ -23,8 +23,10 @@ def main():
 
     collection_filename = mainfolder / "eclair_dataset_2001_designvariables.csv"
 
-    dataframe =  pandas.read_csv(collection_filename, index_col=0)
+    pure_collection_filename = mainfolder / "eclair_dataset_2001_designvariables_constraints_met.csv"
 
+    dataframe =  pandas.read_csv(collection_filename, index_col=0)
+    print("solving rw")
     dataframe["aux_q_pbl_kg_kg"] = dataframe.apply(lambda row:
                                                     LES2emu.solve_rw_lwp( 101780,
                                                                          row["tpot_pbl"],
@@ -32,14 +34,17 @@ def main():
                                                                          row["pbl"]*100.),
                                                                          axis = 1)
     dataframe["aux_q_pbl_g_kg"] = dataframe["aux_q_pbl_kg_kg"]*1e3
-
+    print("solving q constraint")
     dataframe["q_constraint"] = dataframe.apply(lambda row:
                                                         row["aux_q_pbl_g_kg"] - row["q_inv"] > 1 ,
                                                         axis = 1)
-
-    violations = dataframe[dataframe["q_constraint"]].shape[0]
+    print("checking number of violations")
+    violations = dataframe.loc[lambda df: df["q_constraint"] == False].shape[0]
+    pure = dataframe.loc[lambda df: df["q_constraint"] == True]
     total = dataframe.shape[0]
     print(f"constraint violations {violations}, fraction {violations/total}")
+    print("saving pure collection")
+    pure.csv(pure_collection_filename)
 
 
 
