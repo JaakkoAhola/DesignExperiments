a701bf2a95b2ff8b11566cb961295e8acc6cbfe7
Jaakko Ahola <jaakko.ahola@virnex.fi>
2023-07-06 17:34:22 +0300

    init_01.bash added log folder for filldistance

--- init_01.bash
+++ init_01.bash
@@ -3,7 +3,7 @@ mkdir -p data/01_source
 mkdir -p data/02_raw_output data/02_raw_output/design_stats_maxpro data/02_raw_output/design_stats_maximin
 
 mkdir -p data/03_figure_analysis
-mkdir -p batch_job_scripts/logs/R batch_job_scripts/logs/bsp
+mkdir -p batch_job_scripts/logs/R batch_job_scripts/logs/bsp batch_job_scripts/logs/filldistance
 
 
 echo "REPO=${PWD}" | tee .env src_python/.env src_r/.env
