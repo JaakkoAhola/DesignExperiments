5b7a0eba16b5d4d4c79d23a16f859978914301f8
Jaakko Ahola <jaakko.ahola@fmi.fi>
2023-07-28 10:48:45 +0300

    submit_run_lookuptables.bash: more time and and working PYTHONUSERBASE path in puhti

--- batch_job_scripts/submit_run_lookuptables.bash
+++ batch_job_scripts/submit_run_lookuptables.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --job-name=LookUP
 #SBATCH --account=project_2000360
-#SBATCH --time=12:00:00
-#SBATCH --mem-per-cpu=4G
+#SBATCH --time=72:00:00
+#SBATCH --mem-per-cpu=10G
 #SBATCH --partition=small
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
@@ -13,5 +13,6 @@
 module load python-data/3.8-22.10
 echo "module loaded"
 
+export PYTHONUSERBASE=/projappl/project_2000360/project_pip_packages_python-data_3.8-22.10
 cd ../src_python
 srun python run_lookup_table.py
