3faddfd4e03521dea5c2e9ae6e81cc1f646e60bb
Jaakko Ahola <jaakko.ahola@virnex.fi>
2023-07-28 21:26:23 +0300

    batch_job_scripts/submit_job.py: SBATCH bug fixed, cpus-per-task added

--- batch_job_scripts/submit_job.py
+++ batch_job_scripts/submit_job.py
@@ -199,16 +199,17 @@ def get_batch_job_script(parameter_dict):
     argument = get_argument(parameter_dict)
 
     batch_job_script = f"""#!/bin/bash
-# SBATCH --job-name={jobname}
-# SBATCH --account={account}
-# SBATCH --time={walltime}
-# SBATCH --mem-per-cpu={memory}
-# SBATCH --partition=small
-# SBATCH --nodes=1
-# SBATCH --ntasks=1
-# SBATCH --mail-type=END
-# SBATCH --mail-user={email}
-# SBATCH --output={logfile}
+#SBATCH --job-name={jobname}
+#SBATCH --account={account}
+#SBATCH --time={walltime}
+#SBATCH --mem-per-cpu={memory}
+#SBATCH --partition=small
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mail-type=END
+#SBATCH --mail-user={email}
+#SBATCH --output={logfile}
 
 {module_and_setup}
 {command} {argument}
