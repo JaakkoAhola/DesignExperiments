46cef7900863d13576002b58595637afefd7f2c4
Jaakko Ahola <jaakko.ahola@fmi.fi>
2023-08-13 11:24:59 +0300

    batch_job_scripts/submit_with_puhti.bash: sumbit and after short sleep remove the file

--- batch_job_scripts/submit_with_puhti.bash
+++ batch_job_scripts/submit_with_puhti.bash
@@ -4,5 +4,8 @@ export PYTHONUSERBASE=/projappl/project_2000360/project_pip_packages_python-data
 
 python submit_job.py $1
 for i in $(ls temp_*.bash);
-   do echo $i
+   do sbatch $i
+      sleep 5s
+      echo "Removing file $i"
+      rm $i
 done
