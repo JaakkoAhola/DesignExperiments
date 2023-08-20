f04bd8dba6caf7e5df8a25be02d1ebaf9ee65571
Jaakko Ahola <jaakko.ahola@virnex.fi>
2023-08-20 11:12:54 +0300

    Added submit_analyse_figure_distribution.bash a batch job script for getting distribution figure

---
+++ batch_job_scripts/submit_analyse_figure_distribution.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=FIG_DIST
+#SBATCH --account=project_2000360
+#SBATCH --time=01:01:00
+#SBATCH --mem-per-cpu=10G
+#SBATCH --partition=small
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --mail-type=END #uncomment to enable mail
+#SBATCH --mail-user=jjahol@utu.fi
+#SBATCH --output=logs/analyse_figure_distribution.log
+
+module load python-data/3.8-22.10
+echo "module loaded"
+
+export PYTHONUSERBASE=/projappl/project_2000360/project_pip_packages_python-data_3.8-22.10
+cd ../src_python
+srun python analyse_figure_distribution.py
