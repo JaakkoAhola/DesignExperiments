247d9e0e7a46ea1b0ebbe24a754096c49518ec59
Jaakko Ahola <jaakko.ahola@virnex.fi>
2023-08-27 11:27:46 +0300

    reps not as list in yamls

--- batch_job_scripts/submit_job.py
+++ batch_job_scripts/submit_job.py
@@ -267,7 +267,7 @@ def main():
                                    "setname": setname,
                                    "measure": measure,
                                    "account": account,
-                                   "designpoints": int(desing_point),
+                                   "designpoints": desing_point,
                                    "reps": reps,
                                    "email": email}
                     print(submit_dict)
